e9b07d954c9898610c90f871bd5505f62f020f9d
Leo Mendoza Jr <leo@skyturtle.io>
2021-07-27 09:54:25 -0400

    add supervisor for dictionary

--- dictionary/lib/dictionary/application.ex
+++ dictionary/lib/dictionary/application.ex
@@ -3,8 +3,18 @@ defmodule Dictionary.Application do
   Application entry point for the Dictionary application
   """
 
-
   def start(_type, _args) do
-    Dictionary.WordList.start_link()
+    import Supervisor.Spec
+
+    children = [
+      worker(Dictionary.WordList, [])
+    ]
+
+    options = [
+      name: Dictionary.Supervisor,
+      strategy: :one_for_one
+    ]
+
+    Supervisor.start_link(children, options)
   end
 end
