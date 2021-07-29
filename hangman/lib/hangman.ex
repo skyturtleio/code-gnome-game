5495bf3b52fab7063434abc6b58ad20edd89e9cd
Leo Mendoza Jr <leo@skyturtle.io>
2021-07-29 19:55:53 -0400

    refactor Hangman API

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -1,12 +1,14 @@
 defmodule Hangman do
-  alias Hangman.Game
 
-  defdelegate new_game(), to: Game
-  defdelegate tally(game), to: Game
-  defdelegate make_move(game, guess), to: Game
+  def new_game() do
+    Hangman.Server.start_link()
+  end
 
-  def make_move(game, guess) do
-    game = Game.make_move(game, guess)
-    {game, tally(game)}
+  def make_move(game_pid, guess) do
+    GenServer.call(game_pid, {:make_move, guess})
+  end
+
+  def tally(game_pid) do
+    GenServer.call(game_pid, {:tally})
   end
 end
--- hangman/lib/hangman/server.ex
+++ hangman/lib/hangman/server.ex
@@ -14,4 +14,9 @@ defmodule Hangman.Server do
     {game, tally} = Game.make_move(game, guess)
     {:reply, tally, game}
   end
+
+  def handle_call({:tally}, _from, game) do
+    tally = Game.tally(game)
+    {:reply, tally, game}
+  end
 end
