ba9cfe7814fd7dd735f63249dabe4b3f580598f4
Leo Mendoza Jr <leo@skyturtle.io>
2021-08-05 17:52:29 -0400

    add helper to show alert

--- gallows/lib/gallows_web/views/hangman_view.ex
+++ gallows/lib/gallows_web/views/hangman_view.ex
@@ -1,7 +1,31 @@
 defmodule GallowsWeb.HangmanView do
   use GallowsWeb, :view
 
+  @responses %{
+    :won => {:success, "You Won!"},
+    :lost => {:danger, "You Lost!"},
+    :good_guess => {:success, "Good guess!"},
+    :bad_guess => {:warning, "Bad guess!"},
+    :already_used => {:info, "You already guessed that"},
+  }
+
   def words_so_far(tally) do
     tally.letters |> Enum.join(" ")
+  end
+
+  def game_state(state) do
+    @responses[state]
+    |> alert()
+  end
+
+  defp alert(nil), do: ""
+  defp alert({class, message}) do
+    """
+    <div class="alert alert-#{class}">
+      #{message}
+    </div>
+    """
+    |> raw()
+
   end
 end
