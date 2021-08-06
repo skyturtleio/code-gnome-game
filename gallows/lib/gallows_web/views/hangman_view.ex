69f222ab0f94687d88374b67e7adb422d605d699
Leo Mendoza Jr <leo@skyturtle.io>
2021-08-06 19:41:04 -0400

    move game state into helper file

--- gallows/lib/gallows_web/views/hangman_view.ex
+++ gallows/lib/gallows_web/views/hangman_view.ex
@@ -1,6 +1,8 @@
 defmodule GallowsWeb.HangmanView do
   use GallowsWeb, :view
 
+  import GallowsWeb.Views.Helpers.GameStateHelper
+
   def game_over?(%{game_state: game_state}) do
     game_state in [:won, :lost]
   end
@@ -9,31 +11,4 @@ defmodule GallowsWeb.HangmanView do
     button("New Game", to: Routes.hangman_path(conn, :create_game))
   end
 
-  @responses %{
-    :won => {:success, "You Won!"},
-    :lost => {:danger, "You Lost!"},
-    :good_guess => {:success, "Good guess!"},
-    :bad_guess => {:warning, "Bad guess!"},
-    :already_used => {:info, "You already guessed that"},
-  }
-
-  def words_so_far(tally) do
-    tally.letters |> Enum.join(" ")
-  end
-
-  def game_state(state) do
-    @responses[state]
-    |> alert()
-  end
-
-  defp alert(nil), do: ""
-  defp alert({class, message}) do
-    """
-    <div class="alert alert-#{class}">
-      #{message}
-    </div>
-    """
-    |> raw()
-
-  end
 end
